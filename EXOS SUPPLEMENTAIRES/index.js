cbe2755bf5826821eb5bd7f1ac43ff53ba149944
Aziza33 <aitlahcen.aziza@gmail.com>
2025-07-21 12:08:44 +0200

    exercice supplémentaire ud_calculatrice

--- EXOS SUPPLEMENTAIRES/index.js
+++ EXOS SUPPLEMENTAIRES/index.js
@@ -79,36 +79,143 @@
 // const getRandomInteger = () => Math.floor(Math.random() *100)
 // console.log(getRandomInteger())
 
+// ................  EXERCICE CLASS COURS CDA REPEAT  .......................................
 
-class Personnage {
-  constructor (nom, categorie, puissance){
-    this.nom = nom;
-    this.categorie = categorie;
-    this.puissance = puissance
-  }
+// class Personnage {
+//   constructor (nom, categorie, puissance){
+//     this.nom = nom;
+//     this.categorie = categorie;
+//     this.puissance = puissance
+//   }
 
-  // A terminer afficherPersonnage(){
-  //  return personnage;
-  // }
-}
+//   // A terminer afficherPersonnage(){
+//   //  return personnage;
+//   // }
+// }
 
-class Vampire extends Personnage {
+// class Vampire extends Personnage {
   
-  constructor (nom, categorie, puissance, origine, age){
-    super(nom, categorie, puissance);
-    this.origine = origine;
-    this.age = age;
+//   constructor (nom, categorie, puissance, origine, age){
+//     super(nom, categorie, puissance);
+//     this.origine = origine;
+//     this.age = age;
+//   }
+//   afficherVampire(){
+//    return (console.log(`Voici les caractéristiques de ${this.nom} : ${this.categorie}, ${this.puissance}, ${this.origine}, ${this.age}`));
+//   }
+// }
+
+// // const vampire = new Vampire('Dracula', 'Vampire', '1000', 'Pensylvannie', '3000 ans');
+// const dracula = (['Dracula', 'Vampire', '1000', 'Pensylvannie', '3000 ans']);
+
+// const vampire = new Vampire(...dracula);
+
+// // console.log(this.nom, this.categorie, this.puissance);
+
+// vampire.afficherVampire(vampire);
+
+
+
+// .......................  EXERCICE JS UD  .......................................
+
+// number ou parseInt pour transformer le prompt en nombre
+function add(num1, num2){
+return num1 + num2;
+}
+function multiply(num1, num2){
+return num1 * num2;
+}
+function substraction(num1, num2){
+return num1 - num2;
+}
+function divide(num1, num2){
+  if (num2 === 0){
+    throw new Error ("On ne peut pas diviser par 0.");
   }
-  afficherVampire(){
-   return (console.log(`Voici les caractéristiques de ${this.nom} : ${this.categorie}, ${this.puissance}, ${this.origine}, ${this.age}`));
-  }
+return num1 / num2;
 }
 
-// const vampire = new Vampire('Dracula', 'Vampire', '1000', 'Pensylvannie', '3000 ans');
-const dracula = (['Dracula', 'Vampire', '1000', 'Pensylvannie', '3000 ans']);
+let restart = false;
 
-const vampire = new Vampire(...dracula);
+do{
 
-// console.log(this.nom, this.categorie, this.puissance);
+do {
+  var choice = Number(prompt(`Que souhaitez-vous faire ?\n1.Addition \n2-Multiplication\n3-Soustraction\n4-Division\n`));
+}while (choice !=1 && choice !=2 && choice !=3 && choice !=4);
 
-vampire.afficherVampire(vampire);
+do {
+  var num1 = Number(prompt("Entrez un premier nombre"));
+  var num2 = Number(prompt("Entrez un deuxième nombre"));
+}while(isNaN(num1) || isNaN(num2));
+
+try {
+  switch(choice){
+  case 1:
+    var result = add(num1, num2);
+    break;
+    case 2:
+      var result = multiply(num1, num2);
+      break;
+    case 3:
+      var result = substraction(num1, num2);
+      break;
+    case 4:
+      var result = divide(num1, num2);
+      break;
+      default:
+        throw new Error("Une erreur est survenue.");       
+}
+alert("Voici le résultat : " + result);
+
+} catch (error) {
+  alert(error);
+}
+restart = confirm("Souhaitez-vous recommencer un calcul ?");
+} while (restart);
+
+
+
+  // let nombre1 = prompt("Entrez un premier nombre");
+// let nombre2 = prompt("Entrez un deuxième nombre");
+
+  // switch (choice){
+  // case(1):
+  // alert("Le résultat est : " + (nombre1 + nombre2));
+  // break;
+  // case(2):
+  //   alert("Le résultat est : " + (nombre1 * nombre2));
+  // break;
+  // case(3):
+  //   alert("Le résultat est : " + (nombre1 - nombre2));
+  // break;
+  // case(4):
+  //   alert("Le résultat est : " + (nombre1 / nombre2));
+  // break;
+  // };
+
+
+
+
+//  ma façon à tester
+// do {
+  // var choice = Number(prompt(`Que souhaitez-vous faire ?\n1.Addition \n2-Multiplication\n3-Soustraction\n4-Division\n`));
+// let nombre1 = prompt("Entrez un premier nombre");
+// let nombre2 = prompt("Entrez un deuxième nombre");
+
+  // switch (choice){
+  // case(1):
+  // alert("Le résultat est : " + (nombre1 + nombre2));
+  // break;
+  // case(2):
+  //   alert("Le résultat est : " + (nombre1 * nombre2));
+  // break;
+  // case(3):
+  //   alert("Le résultat est : " + (nombre1 - nombre2));
+  // break;
+  // case(4):
+  //   alert("Le résultat est : " + (nombre1 / nombre2));
+  // break;
+  // };
+
+// }while (choice !=1 && choice !=2 && choice !=3 && choice !=4);
+
